da414a1c88182211768352f01bffd3798f244ebc
ahmedhesein1 <ahmedhesin1790@gmail.com>
2025-04-01 21:36:58 +0200

    first commit

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,40 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({
+    type: 'varchar',
+    length: 225,
+    unique: true,
+  })
+  name: string;
+  @Column({
+    type: 'varchar',
+    length: 225,
+    unique: true,
+  })
+  email: string;
+  @Column({
+    type: 'varchar',
+    length: 225,
+    select:false
+  })
+  password: string;
+  @Column({
+    type: 'varchar',
+    length: 225,
+    default: null,
+  })
+  profileImageUrl: string;
+  @CreateDateColumn()
+  createdAt: string;
+  @UpdateDateColumn()
+  updatedAt: string;
+}
