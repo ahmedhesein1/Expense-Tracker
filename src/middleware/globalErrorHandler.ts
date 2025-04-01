09b907d1b4b3161b060d9d5e39fd68fe5dc389a6
ahmedhesein1 <ahmedhesin1790@gmail.com>
2025-04-01 21:37:25 +0200

    first commit

---
+++ src/middleware/globalErrorHandler.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express';
+import AppError from './AppError';
+
+export const globalErrorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  let statusCode = 500;
+  let message = 'Something went wrong';
+  let stack;
+
+  if (err instanceof AppError) {
+    statusCode = err.statusCode;
+    message = err.message;
+    stack = err.stack;
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    stack
+  });
+};
