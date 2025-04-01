22cc0e2874bc1003df9661fb06c27419fde00e7c
ahmedhesein1 <ahmedhesin1790@gmail.com>
2025-04-01 21:36:37 +0200

    first commit

---
+++ src/config/db.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
+import "reflect-metadata";
+import { User } from '../entities/user.entity';
+dotenv.config();
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: Number(process.env.MYSQL_PORT),
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  synchronize: true,
+  entities: [User],
+});
+
