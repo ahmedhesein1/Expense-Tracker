86848ac01a5a5574e32e2f309864e892cabb794f
ahmedhesein1 <ahmedhesin1790@gmail.com>
2025-04-11 00:04:42 +0200

    ADD:income entity

---
+++ src/entities/income.entity.ts
@@ -0,0 +1,44 @@
+import {
+  Entity,
+  Column,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from './user.entity';
+@Entity()
+export class Income {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @ManyToOne(() => User, (user) => user.incomes)
+  user: User;
+  @Column({
+    nullable: false,
+    type: 'varchar',
+    length: 255,
+  })
+  icon: string;
+  @Column({
+    nullable: false,
+    type: 'varchar',
+    length: 255,
+  })
+  source: string;
+  @Column({
+    type: 'decimal',
+    precision:10,
+    scale:2,
+    nullable: false,
+  })
+  amount:number ;
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  date: Date;
+  @CreateDateColumn()
+  createdAt: Date;
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
