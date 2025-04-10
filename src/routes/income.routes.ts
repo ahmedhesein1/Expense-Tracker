df87603b4fbbb7e063be973301e229cdc9145162
ahmedhesein1 <ahmedhesin1790@gmail.com>
2025-04-11 00:00:53 +0200

    Adding and getting Incomes Routes

---
+++ src/routes/income.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { incomeController } from '../controllers/income.controller';
+import { authController } from '../controllers/auth.controller';
+
+export const incomeRoutes = Router();
+
+incomeRoutes.post(
+  '/add-income',
+  authController.protect,
+  incomeController.createIncome,
+);
+incomeRoutes.get(
+  '/',
+  authController.protect,
+  incomeController.getAllIncomes,
+);
