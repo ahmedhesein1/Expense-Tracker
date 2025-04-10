1a7183d1470e49b1112b93dc0840509eb3f4c1d5
ahmedhesein1 <ahmedhesin1790@gmail.com>
2025-04-11 00:03:20 +0200

    Adding create income and getting incomes endpoints

---
+++ src/controllers/income.controller.ts
@@ -0,0 +1,65 @@
+import { Income } from '../entities/income.entity';
+import { Request, Response, NextFunction } from 'express';
+import { AppDataSource } from '../config/db';
+import asyncHandler from 'express-async-handler';
+import dotenv from 'dotenv';
+import { instanceToPlain } from 'class-transformer';
+import AppError from '../middleware/AppError';
+
+dotenv.config();
+class IncomeController {
+  private incomeRepository =
+    AppDataSource.getRepository(Income);
+
+  public createIncome = asyncHandler(
+    async (
+      req: Request,
+      res: Response,
+      next: NextFunction,
+    ) => {
+      const userId = req.user?.id;
+      const { icon, amount, source, date } = req.body;
+      if (!icon || !amount || !source || !date)
+        return next(
+          new AppError('All fields are required', 400),
+        );
+
+      const newIncome = this.incomeRepository.create({
+        user: { id: userId },
+        icon,
+        source,
+        amount: parseFloat(amount),
+        date: new Date(date),
+      });
+      await this.incomeRepository.save(newIncome);
+
+      res.status(201).json({
+        success: true,
+        data: instanceToPlain(newIncome),
+      });
+    },
+  );
+
+  public getAllIncomes = asyncHandler(
+    async (
+      req: Request,
+      res: Response,
+      next: NextFunction,
+    ) => {
+      const incomes = await this.incomeRepository.find({
+        order: {
+          createdAt: 'ASC',
+        },
+      });
+      if (!incomes)
+        return next(
+          new AppError('Could not fins any incomes', 404),
+        );
+      res.status(200).json({
+        success: true,
+        data: instanceToPlain(incomes),
+      });
+    },
+  );
+}
+export const incomeController = new IncomeController();
