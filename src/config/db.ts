443cf67ff1e0129920b504f48d4f7ddb8eb36cab
ahmedhesein1 <ahmedhesin1790@gmail.com>
2025-04-11 22:58:26 +0200

    Add: Expense Entity to DB

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,9 @@
 import dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
-import "reflect-metadata";
+import 'reflect-metadata';
 import { User } from '../entities/user.entity';
 import { Income } from '../entities/income.entity';
+import { Expense } from '../entities/expense.entity';
 dotenv.config();
 export const AppDataSource = new DataSource({
   type: 'mysql',
@@ -12,6 +13,5 @@ export const AppDataSource = new DataSource({
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB,
   synchronize: true,
-  entities: [User,Income],
+  entities: [User, Income, Expense],
 });
-
